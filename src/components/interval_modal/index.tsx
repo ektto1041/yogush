b48fd6d66629da56165eb65a9ed3cc4b65b2aad2
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2023-05-30 13:39:58 +0900

    add interval

---
+++ src/components/interval_modal/index.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+import styles from './interval_modal.module.css';
+import { mdiCheckCircleOutline } from '@mdi/js';
+import { useState } from 'react';
+import Icon from '@mdi/react';
+
+type IntervalmodalProps = {
+  finishInterval: () => void;
+};
+
+export default function IntervalModal({
+  finishInterval,
+}: IntervalmodalProps) {
+  const [interval, setInterval] = useState<number>(1);
+
+  return (
+    <div className={styles.container} >
+      <div className={styles.box} >
+        <div className={styles.title} >
+          주기를 선택해주세요.
+        </div>
+        <div className={styles.contentBox} >
+          <div className={styles.content} >
+            {[1,2,3,4].map(i => (
+              <div className={`${styles.interval}${i === interval ? ' ' + styles.selected : ''}`} onClick={() => setInterval(i)} >
+                {i}달에 한 번
+              </div>
+            ))}
+          </div>
+        </div>
+        <button onClick={finishInterval}>주문</button>
+      </div>
+    </div>
+  );
+};
--- src/pages/my-page/history/index.tsx
+++ src/pages/my-page/history/index.tsx
@@ -3,16 +3,24 @@ import styles from './history.module.css';
 import { useState } from 'react';
 import Header from '@/screens/main/my-page/history/header';
 import Content from '@/screens/main/my-page/history/content';
+import IntervalModal from '@/components/interval_modal';
 
 export async function getServerSideProps() { return { props: {}} }
 export default function History() {
   const [isCardModal, setCardModal] = useState<boolean>(false);
+  const [isIntervalModal, setIntervalModal] = useState<boolean>(true);
+
+  const finishInterval = () => {
+    setIntervalModal(false);
+    setCardModal(true);
+  }
 
   return (
     <div className={styles.container} >
       <Header />
       <Content setCardModal={setCardModal} />
       {isCardModal && (<CardModal />)}
+      {isIntervalModal && (<IntervalModal finishInterval={finishInterval} />)}
     </div>
   )
 }
